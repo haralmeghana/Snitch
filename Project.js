739b05566253fcedb3f6c31f24b3898cf6e2cdb8
haralmeghana <128888658+haralmeghana@users.noreply.github.com>
2023-06-01 13:49:16 +0530

    Add files via upload

---
+++ Project.js
@@ -0,0 +1,120 @@
+function Aaa(){
+    document.getElementById("p").src="./Images/a2-1.webp";
+}
+function Bbb(){
+    document.getElementById("p").src="./Images/a1-1.webp";
+}
+function Aa(){
+    document.getElementById("meg").src="./Images/a2-2.webp";
+}
+function Bb(){
+    document.getElementById("meg").src="./Images/a1-2.webp";
+}
+function Aa1(){
+    document.getElementById("gha").src="./Images/a2-3.webp";
+}
+function Bb1(){
+    document.getElementById("gha").src="./Images/a1-3.webp";
+}
+function Aa2(){
+    document.getElementById("ana").src="./Images/a2-4.webp";
+}
+function Bb2(){
+    document.getElementById("ana").src="./Images/a1-4.webp";
+}
+function Ooo(){
+    document.getElementById("p11").src="./Images/b2-1.webp";
+}
+function Nnn(){
+    document.getElementById("p11").src="./Images/b1-1.webp";
+}
+function Oo(){
+    document.getElementById("gh").src="./Images/b2-2.webp";
+}
+function Nn(){
+    document.getElementById("gh").src="./Images/b1-2.webp";
+}
+function Oo1(){
+    document.getElementById("me").src="./Images/b2-3.webp";
+}
+function Nn1(){
+    document.getElementById("me").src="./Images/b1-3.webp";
+}
+function Oo2(){
+    document.getElementById("an").src="./Images/b2-4.webp";
+}
+function Nn2(){
+    document.getElementById("an").src="./Images/b1-4.webp";
+}
+function Ggg(){
+    document.getElementById("k1").src="./Images/Se-2-s1.webp";
+}
+function Hhh(){
+    document.getElementById("k1").src="./Images/Se-1-s1.webp";
+}
+function Gg(){
+    document.getElementById("k2").src="./Images/Se-2-s2.webp";
+}
+function Hh(){
+    document.getElementById("k2").src="./Images/Se-1-s2.webp";
+}
+function Gg1(){
+    document.getElementById("k3").src="./Images/Se-2-s3.webp";
+}
+function Hh1(){
+    document.getElementById("k3").src="./Images/Se-1-s3.webp";
+}
+function Gg2(){
+    document.getElementById("k4").src="./Images/Se-2-s4.webp";
+}
+function Hh2(){
+    document.getElementById("k4").src="./Images/Se-1-s4.webp";
+}
+function Vvv(){
+    document.getElementById("pi").src="./Images/p2-1.webp";
+}
+function Jjj(){
+    document.getElementById("pi").src="./Images/p1-1.webp";
+}
+function Vv(){
+    document.getElementById("vf").src="./Images/p2-2.webp";
+}
+function Jj(){
+    document.getElementById("vf").src="./Images/p1-2.webp";
+}
+function Vv1(){
+    document.getElementById("bh").src="./Images/p2-3.webp";
+}
+function Jj1(){
+    document.getElementById("bh").src="./Images/p1-3.webp";
+}
+function Vv2(){
+    document.getElementById("hj").src="./Images/p2-4.webp";
+}
+function Jj2(){
+    document.getElementById("hj").src="./Images/p1-4.webp"
+}
+function Uuu(){
+    document.getElementById("fr").src="./Images/p2-5.webp";
+}
+function Iii(){
+    document.getElementById("fr").src="./Images/p1-5.webp";
+}
+function Uu(){
+    document.getElementById("qi").src="./Images/p2-6.webp";
+}
+function Ii(){
+    document.getElementById("qi").src="./Images/p1-6.webp";
+}
+function Uu1(){
+    document.getElementById("oi").src="./Images/p2-7.webp";
+}
+function Ii1(){
+    document.getElementById("oi").src="./Images/p1-7.webp";
+}
+function Uu2(){
+    document.getElementById("fi").src="./Images/p2-8.webp";
+}
+function Ii2(){
+    document.getElementById("fi").src="./Images/p1-8.webp";
+}
